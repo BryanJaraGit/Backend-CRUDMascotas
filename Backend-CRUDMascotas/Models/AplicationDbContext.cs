a88b7bacf1b7869c8d2bb0861da0c749714b0485
BryanJaraGit <brjara77@gmail.com>
2022-09-16 17:49:10 -0500

    Agregar archivos de proyecto.

---
+++ Backend-CRUDMascotas/Models/AplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend_CRUDMascotas.Models
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Mascota> Mascotas { get; set; }
+    }
+}
---
+++ Backend-CRUDMascotas/Models/Mascota.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend_CRUDMascotas.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public string Raza { get; set; }
+        public string Color { get; set; }
+        public int Edad { get; set; }
+        public float Peso { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
